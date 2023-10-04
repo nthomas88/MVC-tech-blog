14142f8043a6e04fb411e33a5867c52a6018cd79
joseph <jomill0723@outlook.com>
2023-10-04 12:45:34 -0400

    updated, and added to controllers file

---
+++ controllers/api/comment.js
@@ -0,0 +1,3 @@
+const router = require('exoress').Router();
+
+module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,3 +1,10 @@
 const router = require('express').Router;
 
+const userRoutes = require('./user-routes');
+const postRoutes = require('./post-routes');
+const { post } = require('..');
+
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+
 module.exports = router;
