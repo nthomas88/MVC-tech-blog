6cc4bd604f1e73e93a19349108f44594f79fd2e8
joseph <jomill0723@outlook.com>
2023-12-05 17:12:32 -0500

    some db changes

--- config/db.js
+++ config/db.js
@@ -1,27 +1,22 @@
-// db.js
+require('dotenv').config(); // Load environment variables
+const { Sequelize } = require('sequelize');
 
-const Sequelize = require('sequelize');
-require('dotenv').config();
+const sequelize = new Sequelize({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  dialect: 'mysql',
+});
 
-let sequelize;
-
-if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-    sequelize = new Sequelize(process.env.DATABASE_URL, {
-        dialect: 'mysql',
-        protocol: 'mysql',
-        logging: true 
-    });
+async function testConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
 }
 
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch((err) => {
-    console.error('Unable to connect to the database:', err);
-  });
-
-module.exports = sequelize;
+module.exports = { sequelize, testConnection };
